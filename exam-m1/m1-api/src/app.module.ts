35bd14f8ebe068964bc883bb96a712a594c9ceb1
Victoria0601 <ds890601@gmail.com>
2024-11-08 14:29:52 +0100

    build relation between book module

--- exam-m1/m1-api/src/app.module.ts
+++ exam-m1/m1-api/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './modules/database/database.module';
+import { BookModule } from './modules/book/book.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [BookModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
